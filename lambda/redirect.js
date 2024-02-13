e2dc88574e8fb81c85d415b89d31e5f487216fea
Hiroaki Suzuki <rirazou05410@gmail.com>
2024-02-13 21:04:27 +0900

    パス別でのリダイレクト処理を追加

--- lambda/redirect.js
+++ lambda/redirect.js
@@ -1,8 +1,22 @@
 function handler(event) {
-  const newUrl = 'https://aws.amazon.com/jp/'
+  const uri = event.request.uri
+  if (isNotRedirect(uri)) return event.request
+
+  const newUri = getNewUri(uri)
   return {
     statusCode: 301,
     statusDescription: 'Moved Permanently',
-    headers: { location: { value: newUrl } },
+    headers: { location: { value: newUri } },
   }
+}
+
+function isNotRedirect(uri) {
+  return uri === '/' || uri === '/index.html'
+}
+
+function getNewUri(uri) {
+  if (uri === '/page1.html') {
+    return 'https://docs.aws.amazon.com/ja_jp/AmazonCloudFront/latest/DeveloperGuide/Introduction.html'
+  }
+  return 'https://aws.amazon.com/jp/'
 }
